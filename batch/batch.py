76dc079ad148858749d47513c949b0b336ca57c3
Isidora Poznanovic <isidorapoznanovic1@gmail.com>
2024-01-25 18:36:15 +0100

    Finished batch

--- batch/batch.py
+++ batch/batch.py
@@ -30,8 +30,12 @@ def preprocess_data(dataframe, table_name):
 
     dataframe = dataframe.withColumnRenamed("magnitudo", "magnitude")
 
-    
     dataframe.write.mode("overwrite").saveAsTable(table_name)
+
+def quiet_logs(sc):
+  logger = sc._jvm.org.apache.log4j
+  logger.LogManager.getLogger("org"). setLevel(logger.Level.ERROR)
+  logger.LogManager.getLogger("akka").setLevel(logger.Level.ERROR)
 
 if __name__ == '__main__':
 
@@ -48,7 +52,8 @@ if __name__ == '__main__':
                                 .enableHiveSupport() \
                                 .getOrCreate()
 
-    print("Trying to read from file..." + HDFS_NAMENODE)
+    
+    quiet_logs(spark)
 
     customSchema = StructType([
         StructField("time", LongType(), True),
@@ -68,28 +73,32 @@ if __name__ == '__main__':
     df = spark.read.format("csv").option("header","true").schema(customSchema).load(
         HDFS_NAMENODE + "/data/Eartquakes-1990-2023.csv")
 
+    print("Raw data picked up")
 
     preprocess_data(df, "preprocessed_eartquakes")
 
+    print("Preprocessing done")
+
     df= spark.table("preprocessed_eartquakes")
 
-    print("---------------------Prva dva reda----------------------")
-    print(df.take(2))
-    print("--------------------------------------------------------")
+    # ### QUERY 1
+    sqlDf = spark.sql("SELECT * \
+        FROM preprocessed_eartquakes \
+        WHERE tsunami <> 0 \
+        ORDER BY significance DESC;")
+    write_df(sqlDf, "b_eartquakes_tsunami_desc_significance")
+    print("Query 1 done")
 
-    print("Picked up data frame...")
-    #df.registerTempTable("moja")
-    sqlDf = spark.sql("SELECT * FROM preprocessed_eartquakes WHERE tsunami <> 0 ORDER BY significance DESC;")
-    sqlDf.show()
-    write_df(sqlDf, "eartquakes_tsunami_desc_significance")
 
-    print("--------------------------------------------------------pokazao")
+    # ### QUERY 2
+    sqlDf = spark.sql("SELECT AVG(magnitude) \
+        FROM preprocessed_eartquakes;")
+    write_df(sqlDf, "b_all_eartquakes_average_magnitude")
+    print("Query 2 done")
 
-    sqlDf = spark.sql("SELECT AVG(magnitude) FROM preprocessed_eartquakes;")
-    sqlDf.show()
-    print("--------------------------------------------------------pokazaoAVG")
-    sqlDf.coalesce(1).write.mode("overwrite").csv("hdfs://namenode:9000/result/AvgMagnitude.csv", header = 'false')
 
+    # ### QUERY 3
+    # ### Frequency od earthquakes per day per state and average magnitude
     sqlDf = spark.sql("SELECT state,\
         COUNT(*) as total_occurrences,\
         COUNT(*) / DATEDIFF(MAX(date), MIN(date)) as frequency_per_day,\
@@ -98,22 +107,13 @@ if __name__ == '__main__':
         WHERE state IS NOT NULL\
         GROUP BY state\
         ORDER BY total_occurrences DESC\
-        LIMIT 10;")
-    write_df(sqlDf, "eartquakes_per_state")
+        LIMIT 50;")
+    write_df(sqlDf, "b_eartquakes_per_state")
+    print("Query 3 done")
 
-    # sqlDf = spark.sql("SELECT\
-    #     state,\
-    #     COUNT(*) as total_occurrences,\
-    #     AVG(depth) as average_depth,\
-    #     MIN(depth) as min_depth,\
-    #     MAX(depth) as max_depth,\
-    #     percentile_approx(depth, 0.5) as median_depth\
-    #     FROM preprocessed_eartquakes\
-    #     WHERE state IS NOT NULL\
-    #     GROUP BY state\
-    #     ORDER BY MAX(depth) DESC;")
-    # write_df(sqlDf, "depth_per_state")# Define a window specification partitioned by 'state' for general statistics
 
+    # ### QUERY 4
+    # ### Total occurrences number, average depth, min depth, max depth of earthquakes per state
     window_spec_median = Window.partitionBy("state").orderBy("depth")
     result_df = df.withColumn("total_occurrences", F.count("*").over(window_spec_median)) \
         .withColumn("average_depth", F.avg("depth").over(window_spec_median)) \
@@ -122,15 +122,188 @@ if __name__ == '__main__':
     result_df = result_df.select("state", "total_occurrences", "average_depth", "min_depth", "max_depth") \
         .dropDuplicates()
     result_df = result_df.withColumn("max_depth", F.round("max_depth"))
-    #result_df = result_df.orderBy(F.desc("max_depth"))
 
-    write_df(result_df, "depth_per_state")
+    write_df(result_df, "b_depth_per_state")
+    print("Query 4 done")
 
+
+    # ### QUERY 5
+    # ### Percent of earthquakes with magnitude bigger than 3 in states
     window_spec = Window.partitionBy("state")
     result_df = df.withColumn("total_earthquakes", F.count("*").over(window_spec)) \
-        .withColumn("earthquakes_gt_3", F.sum(F.when(F.col("magnitudo") > 3, 1).otherwise(0)).over(window_spec))
+        .withColumn("earthquakes_gt_3", F.sum(F.when(F.col("magnitude") > 3, 1).otherwise(0)).over(window_spec))
     result_df = result_df.select("state", "total_earthquakes", "earthquakes_gt_3") \
         .dropDuplicates()
     result_df = result_df.withColumn("ratio_gt_3", F.col("earthquakes_gt_3") / F.col("total_earthquakes"))
 
-    write_df(result_df, "mag_gt3_per_state")
+    write_df(result_df, "b_mag_gt3_per_state")
+    print("Query 5 done")
+
+
+    # ### QUERY 6
+    # ### Yearly occurance of earthquake, earthquake with tsunami 
+    result_df = df.withColumn("year", F.year("date"))
+    result_df = result_df.groupBy("year").agg(F.count("*").alias("total_earthquakes"),
+            F.sum(F.when(F.col("tsunami") == 1, 1).otherwise(0)).alias("earthquakes_with_tsunami"))
+    result_df = result_df.withColumn("percentage_with_tsunami", (F.col("earthquakes_with_tsunami") / F.col("total_earthquakes")) * 100)
+
+    write_df(result_df, "b_yearly_total_earthquakes")
+    print("Query 6 done")
+
+    # ### QUERY 7
+    # ### Average, max magnitude and number of earthquakes with magnitude bigger than 6 for each month in South and North hemisphere
+    result_df = df.withColumn("hemisphere", F.when(F.col("latitude") >= 0, "North").otherwise("South"))
+    result_df = result_df.withColumn("month", F.month("date"))
+    result_df = result_df.groupBy("month", "hemisphere") \
+        .agg(F.avg("magnitude").alias("average_magnitude"),
+        F.max("magnitude").alias("max_magnitude"),
+        F.sum(F.when(F.col("magnitude") > 6, 1).otherwise(0)).alias("count_gt_6_magnitude")
+    )
+
+    write_df(result_df, "b_montly_earthquake_magnitude_hemispheres")
+    print("Query 7 done")
+
+    # ### QUERY 8
+    # ### Pair of earthquakes that occured in 15 minutes in same area
+    result_df = df.filter((F.year("date") == 2019)&(F.month("date") == 1))
+    joined_df = result_df.alias("e1").join(df.alias("e2"),
+                                (F.col("e1.latitude") - F.col("e2.latitude")).between(-0.1, 0.1) &
+                                (F.col("e1.longitude") - F.col("e2.longitude")).between(-0.1, 0.1) &
+                                (F.col("e1.date") != F.col("e2.date")) &
+                                (F.abs(F.unix_timestamp("e1.date") - F.unix_timestamp("e2.date")) <= 900),  # Within 15 minutes
+                                "inner")
+
+    result_df = joined_df.select(
+        F.when(F.col("e1.date") < F.col("e2.date"), F.col("e1.place")).otherwise(F.col("e2.place")).alias("place1"),
+        F.when(F.col("e1.date") < F.col("e2.date"), F.col("e2.place")).otherwise(F.col("e1.place")).alias("place2"),
+        F.least("e1.date", "e2.date").alias("date1"),
+        F.greatest("e1.date", "e2.date").alias("date2"),
+        F.when(F.col("e1.date") < F.col("e2.date"), F.col("e1.magnitude")).otherwise(F.col("e2.magnitude")).alias("magnitude1"),
+        F.when(F.col("e1.date") < F.col("e2.date"), F.col("e2.magnitude")).otherwise(F.col("e1.magnitude")).alias("magnitude2")
+    ).distinct()
+
+    write_df(result_df, "b_pair_of_earthquakes")
+    print("Query 8 done")
+
+    # ### QUERY 9
+    # ### Average occurrences per month in states near equator
+    equator_df = df.filter((F.col("latitude") >= -5) & (F.col("latitude") <= 5))
+    result_df = equator_df.withColumn("month", F.month("date")) \
+        .withColumn("state", F.col("state"))
+    result_df = result_df.groupBy("month", "state") \
+        .agg(F.count("*").alias("occurrences"))
+    result_df = result_df.groupBy("state") \
+        .agg(F.sum("occurrences").alias("total_occurrences"))
+    result_df = result_df.withColumn("average_occurrences_per_month", F.col("total_occurrences") / 12)
+
+    write_df(result_df, "b_equator_earthquakes")
+    print("Query 9 done")
+
+    # ### QUERY 10
+    # ### Magnitude and coordinates of all earthquakes happend on 2.8.2000. year 
+    august_2_2000_df = df.filter((F.year("date") == 2000) & (F.month("date") == 8) & (F.dayofmonth("date") == 2))
+    result_df = august_2_2000_df.select(
+        "latitude",
+        "longitude",
+        "depth",
+        "magnitude"
+    )
+    write_df(result_df, "b_birthday_earthquakes")
+    print("Query 10 done")
+
+    # ### QUERY 11
+    # ### Bigest aftershock
+
+    df_m = df.filter(F.col("magnitude") >= 8)
+    df_a = df.filter(F.col("magnitude") < 8)
+    joined_df = df_m.alias("main").join(df_a.alias("a"),
+                                (F.col("main.latitude") - F.col("a.latitude")).between(-0.1, 0.1) &
+                                (F.col("main.longitude") - F.col("a.longitude")).between(-0.1, 0.1) &
+                                (F.col("main.date") != F.col("a.date")) &
+                                ((F.unix_timestamp("a.date") - F.unix_timestamp("main.date")) > 0) &
+                                ((F.unix_timestamp("a.date") - F.unix_timestamp("main.date")) <= 18000),  # Within 5 hours
+                                "inner")
+    
+    joined_df = joined_df.select(
+        "main.date",
+        "main.magnitude",
+        "main.place",
+        "a.date",
+        "a.magnitude",
+        "a.place"
+    )
+
+    window_spec = Window.partitionBy("main.date", "main.magnitude", "main.place")
+    result_df = joined_df.withColumn("max_magnitude2", F.max("a.magnitude").over(window_spec))
+    result_df = result_df.filter(F.col("a.magnitude") == F.col("max_magnitude2"))
+
+    final_result_df = result_df.select(
+        "main.date",
+        "main.magnitude",
+        "main.place",
+        "a.date",
+        "a.magnitude",
+        "a.place"
+    )
+
+    result_df = final_result_df.select(
+        F.when(F.col("main.date") < F.col("a.date"), F.col("main.place")).otherwise(F.col("a.place")).alias("place_main"),
+        F.when(F.col("main.date") < F.col("a.date"), F.col("a.place")).otherwise(F.col("main.place")).alias("place_aftershock"),
+        F.least("main.date", "a.date").alias("date_main"),
+        F.greatest("main.date", "a.date").alias("date_afershock"),
+        F.greatest("main.magnitude", "a.magnitude").alias("main_magnitude"),
+        F.least("main.magnitude", "a.magnitude").alias("aftershock_magnitude"),
+    ).distinct()
+
+    write_df(result_df, "b_aftershock_earthquakes")
+    print("Query 11 done")
+
+
+    # ### QUERY 12
+    # ### Bigest forshock
+
+    df_m = df.filter(F.col("magnitude") >= 8)
+    df_f = df.filter(F.col("magnitude") < 8)
+    joined_df = df_m.alias("main").join(df_f.alias("f"),
+                                (F.col("main.latitude") - F.col("f.latitude")).between(-0.1, 0.1) &
+                                (F.col("main.longitude") - F.col("f.longitude")).between(-0.1, 0.1) &
+                                (F.col("main.date") != F.col("f.date")) &
+                                ((F.unix_timestamp("main.date") - F.unix_timestamp("f.date")) > 0) &
+                                ((F.unix_timestamp("main.date") - F.unix_timestamp("f.date")) <= 18000),  # Within 5 hours
+                                "inner")
+    
+    joined_df = joined_df.select(
+        "main.date",
+        "main.magnitude",
+        "main.place",
+        "f.date",
+        "f.magnitude",
+        "f.place"
+    )
+
+    window_spec = Window.partitionBy("main.date", "main.magnitude", "main.place")
+    result_df = joined_df.withColumn("max_magnitude2", F.max("f.magnitude").over(window_spec))
+    result_df = result_df.filter(F.col("f.magnitude") == F.col("max_magnitude2"))
+
+    final_result_df = result_df.select(
+        "main.date",
+        "main.magnitude",
+        "main.place",
+        "f.date",
+        "f.magnitude",
+        "f.place"
+    )
+
+    result_df = joined_df.select(
+        F.when(F.col("main.date") > F.col("f.date"), F.col("main.place")).otherwise(F.col("f.place")).alias("place_main"),
+        F.when(F.col("main.date") > F.col("f.date"), F.col("f.place")).otherwise(F.col("main.place")).alias("place_forshock"),
+        F.greatest("main.date", "f.date").alias("date_main"),
+        F.least("main.date", "f.date").alias("date_forshock"),
+        F.greatest("main.magnitude", "f.magnitude").alias("main_magnitude"),
+        F.least("main.magnitude", "f.magnitude").alias("forshock_magnitude"),
+    ).distinct()
+
+    write_df(result_df, "b_forshock_earthquakes")
+    print("Query 12 done")
+
+    print("Batch data processing done")
